9524b4ffcc39ee541582982a580e4eef45184d9a
bry888 <sulfid@tlen.pl>
2018-11-04 17:08:21 +0100

    popr

--- converter/format_category_to_dfp2.py
+++ converter/format_category_to_dfp2.py
@@ -14,50 +14,16 @@ def convert_urls_for_dfp(links):
     links_list = links.strip().split('\r\n') # <-- potrzebne w textarea!!!
     if links_list != ['']:
 
-        alias_to_cat = 'http://discovery-proxy.allegrogroup.com/p/category-alias/category-aliases?tree.name=listing-pl&alias='
-        kat_dict = 'http://discovery-proxy.allegrogroup.com/p/mskip-service/category-paths?id='
-        parents_dict = 'http://discovery-proxy.allegrogroup.com/p/mskip-service/categories?parent.id='
-        home = '954b95b6-43cf-4104-8354-dea4d9b10ddf'
-        home_dict = parents_dict + home
-
-        # build dict Kategorie Działów
-        j = load_service_json(home_dict)
-        home_dict = dict([ (get_rid_of_PL((i['name']).lower().replace(' ', '-')), i['id']) for i in j['categories'] ])
-
-        cat_list = []
-        nums = []
+        some_link = 'http://sth='
 
         try:
-            for url in links_list:
-                if 'dzial' in url:
-                    parent = home_dict[url[url.rfind('/')+1:]]
-
-                    if parent.isnumeric():
-                        cat_list.append(parent)
-                    else:
-                        j = load_service_json(parents_dict+parent)
-                        cat_list += [ i['id'] for i in j['categories'] ]
-
-                else:
-                    url = url.split('?')[0]
-                    num = url[url.rfind('-')+1:]
-                    if num.isnumeric(): # listing with id in url
-                        nums.append(num)
-                    else: # no id in url
-                        j = load_service_json(alias_to_cat+url[url.rfind('/')+1:])
-                        cat_list.append(j['category']['id'])
-
-            # build category chains from lower path categories
-            if nums != []:
-                j = load_service_json(kat_dict + ','.join(nums))
-                cat_list += [ '_'.join([ i['id'] for i in p['path'] if i['id'].isnumeric() ]) for p in j['paths'] ]
-
+            wynik = 0
         except:
             error = url + ' is not a valid category link'
             return (None, error)
 
-        return (','.join([ i+','+i+'_*' for i in cat_list ]), None)
-        # (10_717_257732_*,10_717_257732,11763_*,11763 , ERROR_if_one_raised)
+        return (wynik, None)
+        # (wynik , ERROR_if_one_raised)
 
     else:
         return 'Paste links to convert them'
--- converter/forms.py
+++ converter/forms.py
@@ -2,38 +2,3 @@ from django import forms
 
 class LinkForm(forms.Form):
     input_links = forms.CharField(widget=forms.Textarea(attrs={'rows': 20, 'cols': 120}))
-
-# NIE DZIALA D;D;D;D;D;
-
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-from django.core.validators import RegexValidator
-
-from re import match
-
-#validate_allegro_links = RegexValidator(regex='')
-def validate_allegro_links(link):
-    if match('allegro\.pl\/[a-z-\/]+[0-9]+.*|allegro\.pl\/dzial\/[a-z-]+', link):
-        raise ValidationError(
-            _('%(value)s is not a proper allegro link'),
-            params={'value': value},
-        )
-
-
-class AllegroField(forms.Field):
-    def to_python(self, value):
-        """Normalize data to a list of strings."""
-        # Return an empty list if no input was given.
-        if not value:
-            return []
-        return value.split('\r\n')
-
-    def validate(self, value):
-        """Check if value consists only of valid links."""
-        # Use the parent's handling of required fields, etc.
-        super().validate(value)
-        for link in value:
-            validate_allegro_links(link)
-
-class LinkForm2(forms.Form):
-    input_links = AllegroField(widget=forms.Textarea(attrs={'rows': 20, 'cols': 120}))#, validators=[validate_allegro_links])
